ba0baa6691ebccf8674b576b71c3f072f358c012
Yousefq80 <ysalharby@gmail.com>
2022-11-22 12:05:06 +0300

    Task done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:grade_calculator/GPA';
 
 void main() {
   runApp(const MyApp());
@@ -16,55 +17,6 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: GPA(),
-    );
-  }
-}
-
-class GPA extends StatelessWidget {
-  GPA({Key? Key}) : super(key: Key);
-
-  final controller = TextEditingController();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text("GPA calculater"),
-      ),
-      body: SafeArea(
-        child: Column(
-          children: [
-            TextField(
-              controller: controller,
-              decoration: InputDecoration(
-                hintText: 'Type your score',
-                prefixIcon: Icon(Icons.percent),
-              ),
-            ),
-            Center(
-              child: ElevatedButton(
-                onPressed: () {
-                  int? grade = int.tryParse(controller.text);
-
-                  if (grade! >= 90) {
-                    print("A");
-                  } else if (grade >= 80) {
-                    print("B");
-                  } else if (grade >= 70) {
-                    print("c");
-                  } else if (grade >= 60) {
-                    print("D");
-                  } else {
-                    print("F");
-                  }
-                },
-                child: Text('Calculate'),
-              ),
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
